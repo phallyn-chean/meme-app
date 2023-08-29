15657bb0533f9ae2bc7fc4d91595f4fcf155509a
phallyn-chean <phollyn.chean@student.cadt.edu.kh>
2023-08-29 16:21:46 +0700

    add model

---
+++ lib/models/cart_card_models.dart
@@ -0,0 +1,11 @@
+class CartCardModels {
+  late String id;
+  late String nameCart;
+  late String imageUrlCart;
+
+  CartCardModels({
+    this.id = '',
+    this.nameCart = '',
+    this.imageUrlCart = '',
+  });
+}
